c51c4f21c81c21c42f24d3141e5b208576bb7a72
JiangWeixian <saber_wei@163.com>
2018-02-26 15:24:10 +0800

    remove const

--- src/router/index.js
+++ src/router/index.js
@@ -13,18 +13,18 @@ import Hello from '../components/Hello'
 
 Vue.use(Router)
 
-const routes = [
-    { path: '/', name: 'Hello', component: Hello }
-    // { path: '/div2img', name: 'Div2Img', component: Div2Img},
-    // { path: '/OnePaper', name: 'neo', component: neo, },
-    // { path: '/OnePaper/cityInfo', name: 'city', component: City}, 
-    // { path: '/OnePaper/shareCard', name: 'shareCard', component: OnePaperShareCard}, 
-    // { path: '/OnePaper/loginBall', name: 'loginBall', component: OnePaperLoginBall }, 
-    // { path: '/OnePaper/weather', name: 'weather', component: OnePaperWeather }, 
-    // { path: '/OnePaper/beginWithDogs', name: 'dogs', component: OnePaperBeginWithDogs }  
-]
+// const routes = [
+//     { path: '/', name: 'Hello', component: Hello }
+//     { path: '/div2img', name: 'Div2Img', component: Div2Img},
+//     { path: '/OnePaper', name: 'neo', component: neo, },
+//     { path: '/OnePaper/cityInfo', name: 'city', component: City}, 
+//     { path: '/OnePaper/shareCard', name: 'shareCard', component: OnePaperShareCard}, 
+//     { path: '/OnePaper/loginBall', name: 'loginBall', component: OnePaperLoginBall }, 
+//     { path: '/OnePaper/weather', name: 'weather', component: OnePaperWeather }, 
+//     { path: '/OnePaper/beginWithDogs', name: 'dogs', component: OnePaperBeginWithDogs }  
+// ]
 
 export default new Router({
   mode: 'history',
-  routes
+  routes: [{ path: '/', name: 'Hello', component: Hello }]
 })
